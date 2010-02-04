4d66d7c4dde27f2c4f35f7bc12eebe8e1f1ba632
Clark Gaebel <cg.wowus.cg@gmail.com>
2010-02-03 19:39:14 -0500

    Prettified the output. And gave props where props were due.

--- Cache Line Detection/main.c
+++ Cache Line Detection/main.c
@@ -4,17 +4,20 @@
 #include "cache.h"
 #include "format.h"
 
+/* Inspired by: http://igoro.com/archive/gallery-of-processor-cache-effects/ */
 int main(int argc, wchar_t** argv)
 {
 	/* One megabyte. Surely our cache is smaller than that! */
 	unsigned int cacheLine = get_cache_line(1*1024*1024);
 	struct size_of_data formattedResult = unitfy_data_size(cacheLine);
 
-	printf_s("Cache line: %u %s\n", formattedResult.quantity, formattedResult.unit);
+	printf_s("Cache line -> [%u%s]\n", formattedResult.quantity, formattedResult.unit);
 
 	/* My cache line is 64kb.. what's yours? */
 	if(cacheLine == 65536)
-		printf_s("\nHeeeey, that's my cache line too! <3\n\n");
+		printf_s("\nHeeeey, that's my cache line too! <3\n");
+
+	printf_s("\n");
 
 	system("PAUSE");
 	return 0;
